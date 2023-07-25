f129bed34fe7f7353d87e1b8a72d6586513a7d86
VitaliyGavrilov <vitaliy140798gavrilov@mail.ru>
2023-07-25 14:56:18 +0300

    +

--- routes/router.js
+++ routes/router.js
@@ -2,13 +2,13 @@
 const router = require('express').Router();// создаем роутре
 const cardRouter = require('./cards');// импорт роута карточек
 const userRouter = require('./users');// импорт роута пользователя
-const { NOT_FOUND } = require('../const/error');// импорт ошибки
+const NotFoundError = require('../errors/not-found-err');// импорт ошибки
 // Обьединяем роуты
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 // На случай запроса на неправильный путь
-router.use('*', (req, res) => {
-  res.status(NOT_FOUND).send({ message: 'Страница не найдена' });
+router.use('*', () => {
+  throw new NotFoundError('Страница не найдена');
 });
 // Экспорт биг-роута
 module.exports = router;
